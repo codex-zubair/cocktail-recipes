e07d81b322ea413e2f0e7d675d10bc8c921d763e
Rocko <codingwithrock@gmail.com>
2022-09-22 22:15:35 +0600

    added read recipe button

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ const ShowAllCocktails = ()=>
     .then(cocktailsList=> setCocktailsList(cocktailsList.drinks))
   },[])
   
-  
   return cockTailsList.map(cockTail=> <Card key = {cockTail.idDrink} cockTail = {cockTail}></Card>)
 
 }
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
 
     const { strDrinkThumb, strDrink } = props.cockTail
-   
+
 
 
     return (
@@ -20,8 +20,17 @@ const Card = (props) => {
 
                 {/* <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p> */}
 
+
+
+
+                <button className='mt-2 inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+                    Read Recipe
+                    <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
             </div>
+
         </div>
+
 
 
     );
